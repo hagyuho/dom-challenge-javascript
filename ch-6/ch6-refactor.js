fe2c3060b21095270e450adc830ff7810bc3c4db
gyuho <hagyuho90@gmail.com>
2022-07-14 13:13:19 +0900

    ch-6 refactoring

--- ch-6/ch6-refactor.js
+++ ch-6/ch6-refactor.js
@@ -6,94 +6,109 @@ var que = [];
 function CreateView(zoneNum) {
   //1. create base View
   zoneNumber = zoneNum;
-  const scoreView = document.querySelector("#scoreArea");
-  const nowScoreView = document.createElement("div");
-  const highScoreView = document.createElement("div");
-  nowScoreView.classList.add("nowScore");
-  highScoreView.classList.add("highScore");
-  nowScoreView.textContent = `Score: ${nowScore}`;
-  highScoreView.textContent = `High Score: ${highScore}`;
+  const $scoreArea = document.querySelector("#scoreArea");
+  const $nowScoreView = document.createElement("div");
+  const $highScoreView = document.createElement("div");
+  $nowScoreView.classList.add("nowScore");
+  $highScoreView.classList.add("highScore");
+  $nowScoreView.textContent = `Score: ${nowScore}`;
+  $highScoreView.textContent = `High Score: ${highScore}`;
 
-  scoreView.appendChild(nowScoreView);
-  scoreView.appendChild(highScoreView);
+  $scoreArea.appendChild($nowScoreView);
+  $scoreArea.appendChild($highScoreView);
 
-  const clickZone = document.querySelector("#zoneArea");
+  const $zoneArea = document.querySelector("#zoneArea");
   for (let i = 0; i < zoneNum; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.dataset.key = i;
-    clickZone.appendChild(box);
+    const $box = document.createElement("div");
+    $box.classList.add("box");
+    $box.dataset.key = i;
+    $zoneArea.appendChild($box);
   }
 
-  const button = document.querySelector("#buttonArea");
-  const buttonView = document.createElement("button");
-  buttonView.textContent = "START";
-  buttonView.classList.add("button");
-  button.appendChild(buttonView);
+  const $buttonArea = document.querySelector("#buttonArea");
+  const $button = document.createElement("button");
+  $button.textContent = "START";
+  $button.classList.add("button");
+  $buttonArea.appendChild($button);
 
-  //2. create addEventListener
-  button.addEventListener("click", handleButtonClick);
-  clickZone.addEventListener("click", handleBoxClick);
+  //2. add EventListener
+  $buttonArea.addEventListener("click", handleButtonClick);
+  $zoneArea.addEventListener("click", handleBoxClick);
 }
 
+//event : when button is clicked
+const handleButtonClick = () => {
+  const $button = document.querySelector(".button");
+  $button.setAttribute("disabled", true);
+  startQuiz(nowScore + 1);
+};
+
+//event : when box is clicked
 const handleBoxClick = (event) => {
-  const boxKey = +event.target.dataset.key;
-  const answer = que.shift();
-  let isOk = answer === boxKey ? true : false;
-  if (!isOk) {
+  const clickedNum = +event.target.dataset.key;
+  const answerNum = que.shift();
+  let isCorrect = answerNum === clickedNum ? true : false;
+
+  if (!isCorrect) {
     que = [];
-    const button = document.querySelector(".button");
-    button.removeAttribute("disabled");
-
     nowScore = 0;
-    const nowScoreView = document.querySelector(".nowScore");
-    nowScoreView.textContent = `Score: ${nowScore}`;
-
+    const $button = document.querySelector(".button");
+    const $zoneArea = document.querySelector("#zoneArea");
+    const $nowScore = document.querySelector(".nowScore");
+    $button.removeAttribute("disabled");
+    $zoneArea.classList.add("shake");
+    $nowScore.textContent = `Score: ${nowScore}`;
     event.target.classList.add("red");
-    const clickZone = document.querySelector("#zoneArea");
-    clickZone.classList.add("shake");
+
     setTimeout(() => {
-      clickZone.classList.remove("shake");
+      $zoneArea.classList.remove("shake");
       event.target.classList.remove("red");
     }, 500);
-  } else {
+  }
+
+  if (isCorrect) {
     event.target.classList.add("green");
     setTimeout(() => {
       event.target.classList.remove("green");
     }, 100);
 
     if (que.length === 0) {
-      nowScore++;
-      let nowScoreView = document.querySelector(".nowScore");
-      nowScoreView.textContent = `Score: ${nowScore}`;
-      if (nowScore > highScore) {
-        highScore = nowScore;
-        let highScoreView = document.querySelector(".highScore");
-        highScoreView.textContent = `High Score: ${highScore}`;
-      }
+      scoreChange();
       startQuiz(nowScore + 1);
     }
   }
 };
 
-const handleButtonClick = () => {
-  const button = document.querySelector(".button");
-  button.setAttribute("disabled", true);
-  startQuiz(nowScore + 1);
+//event : when quiz finally succeeded
+// 1. nowScore ++ and render
+// 2. update highScore and render
+const scoreChange = () => {
+  nowScore++;
+  let $nowScore = document.querySelector(".nowScore");
+  $nowScore.textContent = `Score: ${nowScore}`;
+  if (nowScore > highScore) {
+    highScore = nowScore;
+    let $highScore = document.querySelector(".highScore");
+    $highScore.textContent = `High Score: ${highScore}`;
+  }
 };
 
+//event : when button is clicked / when quiz finally succeeded
+// 1. generate random number
+// 2. show which box is matched with random number (box's color changes momentarily )
+// 3. push the random number into queue
+// 4. repeat 1-3
 const startQuiz = (num) => {
-  const clickZone = document.querySelector("#zoneArea");
-  const boxes = clickZone.getElementsByClassName("box");
-
+  const $zoneArea = document.querySelector("#zoneArea");
+  const $boxes = $zoneArea.getElementsByClassName("box");
   for (let i = 0; i < num; i++) {
     const ramdomNumber = Math.floor(Math.random() * zoneNumber);
     setTimeout(() => {
-      for (let j = 0; j < boxes.length; j++) {
-        if (ramdomNumber === +boxes[j].dataset.key) {
-          boxes[j].classList.add("blue");
+      for (let j = 0; j < $boxes.length; j++) {
+        if (ramdomNumber === +$boxes[j].dataset.key) {
+          $boxes[j].classList.add("blue");
           setTimeout(() => {
-            boxes[j].classList.remove("blue");
+            $boxes[j].classList.remove("blue");
           }, 500);
         }
       }
